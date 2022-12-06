4bfa6a1df9ed23243304c7637b2479784b25e20e
Kirill Fedoseev <k.fedoseev@innopolis.university>
2022-12-06 17:00:39 +0400

    Add missing sanity checks

--- src/BobVault.sol
+++ src/BobVault.sol
@@ -2,6 +2,7 @@
 
 pragma solidity 0.8.15;
 
+import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./yield/YieldConnector.sol";
@@ -57,6 +58,7 @@ contract BobVault is EIP1967Admin, Ownable, YieldConnector {
     event Give(address indexed token, uint256 amount);
 
     constructor(address _bobToken) {
+        require(Address.isContract(_bobToken), "BobVault: not a contract");
         bobToken = IERC20(_bobToken);
     }
 
