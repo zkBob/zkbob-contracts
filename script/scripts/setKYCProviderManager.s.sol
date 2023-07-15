cc3db508def3c39cc06ffd723d264e77858df84c
Alexander Kolotov <alexandr.kolotov@gmail.com>
2023-07-15 19:32:43 +0000

    more robust script

--- script/scripts/setKYCProviderManager.s.sol
+++ script/scripts/setKYCProviderManager.s.sol
@@ -6,32 +6,60 @@ import "forge-std/Script.sol";
 import "../../src/zkbob/ZkBobPool.sol";
 
 contract SetManagerAndTierLimits is Script {
-    function run() external {
-        ZkBobPool.Limits memory limits;
-        ZkBobPool pool = ZkBobPool(0x3bd088C19960A8B5d72E4e01847791BD0DD1C9E6);
+    uint256 internal constant MAX_DENOMINATOR = 0x8000000000000000000000000000000000000000000000000000000000000000 - 1;
 
-        //vm.startPrank(0x2d5C035F99a7DF3067EDAcDED0e117d7076aBf7c);
+    function tokenToPool(uint256 _value, uint256 _denominator) internal pure returns (uint64) {
+        if (_denominator <= MAX_DENOMINATOR) {
+            return uint64(_value / _denominator);
+        } else {
+            return uint64(_value * (_denominator & MAX_DENOMINATOR));
+        }
+    }
+
+    function poolToToken(uint256 _value, uint256 _denominator) internal pure returns (uint256) {
+        if (_denominator <= MAX_DENOMINATOR) {
+            return _value * _denominator;
+        } else {
+            return _value / (_denominator & MAX_DENOMINATOR);
+        }
+    }
+
+    function run() external {
+        ZkBobPool pool = ZkBobPool(0x49661694a71B3Dab9F25E86D5df2809B170c56E6); // Goerli, BOB->USDC pool
+        // ZkBobPool pool = ZkBobPool(0xCF6446Deb67b2b56604657C67DAF54f884412531);  // Goerli, USDC pool
+        IKycProvidersManager kyc_mgr = IKycProvidersManager(0x2C34aFcB1c51796c3c0C7710c72a56Eb72E1E81D);
+        address user = address(0xBF3d6f830CE263CAE987193982192Cd990442B53);
+        address owner = address(0x39F0bD56c1439a22Ee90b4972c16b7868D161981);
+
+        ZkBobPool.Limits memory limits_prev;
+        ZkBobPool.Limits memory limits_new;
+
+        uint256 denominator = pool.denominator();        
+        limits_prev = pool.getLimitsFor(user);
+
+        // vm.startPrank(owner);
         vm.startBroadcast();
 
         pool.setLimits({
             _tier: 254,
-            _tvlCap: 1000000 ether,
-            _dailyDepositCap: 300000 ether,
-            _dailyWithdrawalCap: 100000 ether,
-            _dailyUserDepositCap: 20000 ether,
-            _depositCap: 20000 ether,
-            _dailyUserDirectDepositCap: 10000 ether,
-            _directDepositCap: 1000 ether
+            _tvlCap: poolToToken(limits_prev.tvlCap, denominator),
+            _dailyDepositCap: poolToToken(limits_prev.dailyDepositCap, denominator),
+            _dailyWithdrawalCap: poolToToken(limits_prev.dailyWithdrawalCap, denominator),
+            _dailyUserDepositCap: poolToToken(limits_prev.dailyUserDepositCap, denominator) * 2,
+            _depositCap: poolToToken(limits_prev.depositCap, denominator) * 2,
+            _dailyUserDirectDepositCap: poolToToken(limits_prev.dailyUserDirectDepositCap, denominator),
+            _directDepositCap: poolToToken(limits_prev.directDepositCap, denominator)
         });
 
-        pool.setKycProvidersManager(IKycProvidersManager(0x98DB3A72BeF2145A8F8d8B94F81317341Af2b08C));
+        pool.setKycProvidersManager(kyc_mgr);
 
         vm.stopBroadcast();
-        //vm.stopPrank();
+        // vm.stopPrank();
 
-        require(pool.kycProvidersManager() == IKycProvidersManager(0x98DB3A72BeF2145A8F8d8B94F81317341Af2b08C), "Incorrect KYC Provider Manager");
+        require(pool.kycProvidersManager() == kyc_mgr, "Incorrect KYC Provider Manager");
 
-        limits = pool.getLimitsFor(0x84E94F8032b3F9fEc34EE05F192Ad57003337988);
-        require((limits.dailyUserDepositCap == 20000 gwei) && (limits.depositCap == 20000 gwei), "Incorrect limits");
+        limits_new = pool.getLimitsFor(user);
+        require((limits_new.tvlCap == limits_prev.tvlCap) && (limits_new.dailyWithdrawalCap == limits_prev.dailyWithdrawalCap), "Incorrect limits");
+        require((limits_new.dailyUserDepositCap == 2 * limits_prev.dailyUserDepositCap) && (limits_new.depositCap == 2 * limits_prev.depositCap), "Incorrect KYC limits");
     }
 }
