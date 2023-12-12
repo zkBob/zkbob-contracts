eeb3821801499794fdd283149464274a7bead6f7
Alexander Filippov <aleksander.fill@gmail.com>
2023-12-12 16:17:41 +0300

    Add AllowListOperatorManager tests

--- src/zkbob/manager/AllowListOperatorManager.sol
+++ src/zkbob/manager/AllowListOperatorManager.sol
@@ -2,38 +2,33 @@
 
 pragma solidity 0.8.15;
 
-import "../../interfaces/IOperatorManager.sol";
-import "../../utils/Ownable.sol";
+import {IOperatorManager} from "../../interfaces/IOperatorManager.sol";
+import {Ownable} from "../../utils/Ownable.sol";
 
 /**
  * @title AllowListOperatorManager
- * @dev Implements a allow list based access control for ZkBobPool relayers.
+ * @dev Implements an allow list based access control for ZkBobPool relayers.
  */
 contract AllowListOperatorManager is IOperatorManager, Ownable {
     // if true, only whitelisted addresses can be operators
     // if false, anyone can be an operator
     bool public allowListEnabled;
-    
+
     // mapping of whitelisted operator addresses
     mapping(address => bool) public operators;
 
-    // mapping of all historical fee receiver addresses, we keep fee receivers addresses
-    // in a mapping to allow fee withdrawals even after operator address was changed
+    // mapping of fee receivers for operators
     mapping(address => address) public operatorFeeReceiver;
 
     event UpdateOperator(address indexed operator, address feeReceiver, bool allowed);
     event UpdateAllowListEnabled(bool enabled);
 
     modifier nonZeroAddress(address addr) {
-        require(addr != address(0), "AllowListOperatorManager: zero address");
+        require(addr != address(0), "OperatorManager: zero address");
         _;
     }
 
-    constructor(
-        address[] memory _operators, 
-        address[] memory _feeReceivers, 
-        bool _allowListEnabled
-    ) Ownable() {
+    constructor(address[] memory _operators, address[] memory _feeReceivers, bool _allowListEnabled) Ownable() {
         allowListEnabled = _allowListEnabled;
         bool[] memory _allowed = new bool[](_operators.length);
         for (uint256 i = 0; i < _operators.length; i++) {
@@ -41,61 +36,96 @@ contract AllowListOperatorManager is IOperatorManager, Ownable {
         }
         _setOperators(_operators, _allowed, _feeReceivers);
     }
-    
+
+    /**
+     * @dev Doesn't return any data, as operator URI is not used in this implementation.
+     */
     function operatorURI() external pure returns (string memory) {
         return "";
     }
 
+    /**
+     * @dev Sets the allow list enabled flag.
+     * @param _allowListEnabled flag to enable or disable allow list.
+     */
     function setAllowListEnabled(bool _allowListEnabled) external onlyOwner {
         allowListEnabled = _allowListEnabled;
         emit UpdateAllowListEnabled(_allowListEnabled);
     }
 
-    function setOperators(
-        address[] calldata _operators, 
-        bool[] calldata _allowed,
-        address[] calldata _feeReceivers
-    ) external onlyOwner {
-        _setOperators(_operators, _allowed, _feeReceivers);
-    }
-
-    function setOperator(
-        address _operator, 
-        address _feeReceiver, 
-        bool _allowed
-    ) external onlyOwner {
+    /**
+     * @dev Adds or removes an operator from the allow list.
+     * @param _operator address of the operator.
+     * @param _allowed flag to enable or disable operator.
+     * @param _feeReceiver address of the fee receiver.
+     */
+    function setOperator(address _operator, address _feeReceiver, bool _allowed) external onlyOwner {
         _setOperator(_operator, _allowed, _feeReceiver);
     }
 
+    /**
+     * @dev Adds or removes operators from the allow list.
+     * @param _operators addresses of the operators.
+     * @param _allowed flags to enable or disable operators.
+     * @param _feeReceivers addresses of the fee receivers.
+     */
+    function setOperators(
+        address[] calldata _operators,
+        bool[] calldata _allowed,
+        address[] calldata _feeReceivers
+    )
+        external
+        onlyOwner
+    {
+        _setOperators(_operators, _allowed, _feeReceivers);
+    }
+
+    /**
+     * @dev Sets the fee receiver for the operator.
+     * @param _feeReceiver address of the fee receiver.
+     */
+    function setFeeReceiver(address _feeReceiver) external {
+        require(isOperator(msg.sender), "OperatorManager: operator not allowed");
+        operatorFeeReceiver[msg.sender] = _feeReceiver;
+        emit UpdateOperator(msg.sender, _feeReceiver, true);
+    }
+
     function _setOperators(
-        address[] memory _operators, 
+        address[] memory _operators,
         bool[] memory _allowed,
         address[] memory _feeReceivers
-    ) internal {
-        require(_operators.length == _feeReceivers.length, "AllowListOperatorManager: arrays length mismatch");
-        require(_operators.length == _allowed.length, "AllowListOperatorManager: arrays length mismatch");
-        
+    )
+        internal
+    {
+        require(_operators.length == _feeReceivers.length, "OperatorManager: arrays length mismatch");
+        require(_operators.length == _allowed.length, "OperatorManager: arrays length mismatch");
+
         for (uint256 i = 0; i < _operators.length; i++) {
             _setOperator(_operators[i], _allowed[i], _feeReceivers[i]);
         }
     }
 
-    function _setOperator(
-        address _operator, 
-        bool _allowed,
-        address _feeReceiver
-    ) nonZeroAddress(_operator) internal {
+    function _setOperator(address _operator, bool _allowed, address _feeReceiver) internal nonZeroAddress(_operator) {
         operators[_operator] = _allowed;
         if (_feeReceiver != address(0) && _allowed) {
             operatorFeeReceiver[_operator] = _feeReceiver;
         }
-        emit UpdateOperator(_operator, _feeReceiver, _allowed);
+        emit UpdateOperator(_operator, operatorFeeReceiver[_operator], _allowed);
     }
 
-    function isOperator(address _addr) external view override returns (bool) {
+    /**
+     * @dev Returns true if the address is an operator.
+     * @param _addr address to check.
+     */
+    function isOperator(address _addr) public view override returns (bool) {
         return operators[_addr] || !allowListEnabled;
     }
 
+    /**
+     * @dev Returns true if the address is an operator fee receiver.
+     * @param _operator address of the operator.
+     * @param _addr address to check.
+     */
     function isOperatorFeeReceiver(address _operator, address _addr) external view override returns (bool) {
         return operatorFeeReceiver[_operator] == _addr;
     }
